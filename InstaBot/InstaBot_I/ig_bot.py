727f439696ac02bf105f07da7a2ddf3c9d9d85a4
Vishal Soni <84185041+vishals21iitk@users.noreply.github.com>
2022-12-01 18:26:24 +0530

    Update ig_bot.py

--- InstaBot/InstaBot_I/ig_bot.py
+++ InstaBot/InstaBot_I/ig_bot.py
@@ -13,14 +13,14 @@ def login(username_, password_):
 
     # accepting the cookies
     try:
-        cookies_accept= driver.find_element_by_xpath('//button[text()="Accept"]')
+        cookies_accept= driver.find_element(By.XPATH,'//button[text()="Accept"]')
         cookies_accept.click()
         time.sleep(1)
     except:
         pass
 
-    username = driver.find_element_by_name('username')
-    password = driver.find_element_by_name('password')
+    username = driver.find_element(By.NAME,'username')
+    password = driver.find_element(By.NAME,'password')
 
     username.click()
     username.send_keys(username_)
@@ -30,13 +30,13 @@ def login(username_, password_):
     password.send_keys(password_)
     time.sleep(1)
 
-    log_in=driver.find_element_by_xpath('//div[text()="Log In"]')
+    log_in=driver.find_element(By.XPATH,'//div[text()="Log In"]')
     log_in.click()
     time.sleep(4)
 
     # credential storage
     try:
-        credentials= driver.find_element_by_xpath('//button[text()="Not Now"]')
+        credentials= driver.find_element(By.XPATH,'//button[text()="Not Now"]')
         credentials.click()
         time.sleep(1)
     except:
@@ -44,7 +44,7 @@ def login(username_, password_):
 
     #  notifications
     try:
-        notifications = driver.find_element_by_xpath('//button[text()="Not Now"]')
+        notifications = driver.find_element(By.XPATH,'//button[text()="Not Now"]')
         notifications.click()
         time.sleep(1)
     except:
@@ -56,11 +56,11 @@ def follow_like_comment(number):
 
     for i in range (len(profiles_list)): # 2 pages
 
-        search_1 = driver.find_element_by_xpath('//span[text()="Search"]')
+        search_1 = driver.find_element(By.XPATH,'//span[text()="Search"]')
         search_1.click()
         time.sleep(1)
 
-        search_2 = driver.find_element_by_xpath('//input[@placeholder="Search"]')
+        search_2 = driver.find_element(By.XPATH,'//input[@placeholder="Search"]')
         search_2.send_keys(profiles_list[i])
 
         count = 0
@@ -70,42 +70,42 @@ def follow_like_comment(number):
             time.sleep(1)
 
         # follow
-        follow = driver.find_element_by_xpath('//button[text()="Follow"]')
+        follow = driver.find_element(By.XPATH,'//button[text()="Follow"]')
         follow.click()
         time.sleep(2)
 
         # first media element
-        media_element = driver.find_element_by_xpath('//div[@class="eLAPa"]')
+        media_element = driver.find_element(By.XPATH,'//div[@class="eLAPa"]')
         media_element.click()
         time.sleep(2)
 
         for k in range (number):
             # like
-            like = driver.find_element_by_xpath('//button[@class="wpO6b "]//*[@aria-label="Like"]')
+            like = driver.find_element(By.XPATH,'//button[@class="wpO6b "]//*[@aria-label="Like"]')
             like.click()
             time.sleep(1)
             # comment
-            comment_1 = driver.find_element_by_xpath('//div[@class="RxpZH"]')
+            comment_1 = driver.find_element(By.XPATH,'//div[@class="RxpZH"]')
             comment_1.click()
             time.sleep(1)
 
-            comment_2 = driver.find_element_by_class_name('Ypffh')
+            comment_2 = driver.find_element(By.CLASS_NAME,'Ypffh')
             comment_2.send_keys(random.choice(comments))
             time.sleep(1)
             # post
-            post = driver.find_element_by_xpath('//button[text()="Post"]')
+            post = driver.find_element(By.XPATH,'//button[text()="Post"]')
             post.click()
             time.sleep(1)
             # click on next media element
-            next_button = driver.find_element_by_xpath('//a[text()="Next"]')
+            next_button = driver.find_element(By.XPATH,('//a[text()="Next"]')
             next_button.click()
             time.sleep(1)
 
-        close = driver.find_element_by_xpath('//*[@aria-label="Close"]')
+        close = driver.find_element(By.XPATH,'//*[@aria-label="Close"]')
         close.click()
 
 
 
 # function call
-login('testuseraccount24', 'testuser')
+login('sonivishal_877', 'password')
 follow_like_comment(3)
